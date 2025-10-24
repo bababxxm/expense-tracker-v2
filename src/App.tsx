97f68afb2cf12d385d5a0ade85abf6d1dfc3063a
Sorawit Klaokliang <sklaokli@z2t18c2.42bangkok.com>
2025-10-24 19:05:06 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Provider } from "react-redux";
+import store from "./store/store";
+
 function App() {
   return (
-    <>
-      <h1 className="font-bold text-4xl">Hello</h1>
-    </>
+    <Provider store={store}>
+      <div>
+        <h1 className="font-bold text-4xl">Hello</h1>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/store/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import expenseReducer from "../slice/expenseSlice";
+
+export default configureStore({
+  reducer: {
+    expense: expenseReducer,
+  },
+});
