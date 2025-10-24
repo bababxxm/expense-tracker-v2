9dc928bf46773e783aefc40ba5b1e1568d51e34c
Sorawit Klaokliang <sklaokli@z2t18c2.42bangkok.com>
2025-10-24 19:06:48 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Provider } from "react-redux";
 import store from "./store/store";
+import ExpenseHeader from "./components/ExpenseHeader";
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <h1 className="font-bold text-4xl">Hello</h1>
+      <div className="flex justify-center items-center">
+        <div className="w-[60%] py-4">
+          <ExpenseHeader />
+          {/* <AddExpense /> */}
+          <br></br>
+          {/* <ExpenseTable /> */}
+          <br></br>
+        </div>
       </div>
     </Provider>
   );
